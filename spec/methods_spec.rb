1bc365df80bce98e497c1c02abf6eb11e462db72
Rosa <rosarenee@att.net>
2021-08-10 03:52:06 -0500

    eh all but the last one

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,28 @@ describe '#halve' do
   end
 
 end
+
+
+def greet_programmer()
+  puts "Hello, programmer!" 
+end
+
+def greet(name="Naureen")
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+  
+  puts "Hello, #{name}!"
+  
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def halve(num1=10, num2=2)
+  # if (typeof number !== "number") 
+  #   return null
+  return num1 / num2
+end
